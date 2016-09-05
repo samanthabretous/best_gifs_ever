b6b1fee701210e626786e71dda68c00a8893575c
Samantha Bretous <sam@samanthabretous.com>
2016-09-05 17:36:40 -0400

    loaded images with ajax. styled css

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 function loadGifs(response){
-  console.log(response)
-  var image = document.querySelector('.test')
-  image.setAttribute('src', 'www.reddit.com')
-  console.log(image)
+    var gifs = document.querySelector('.gifs');
+
+    //create element to place random gif
+    var imageDiv = document.createElement('div');
+    var image = document.createElement('img');
+
+    //set the image src to the ajax response
+    var imageUrl = response.data.fixed_width_downsampled_url;
+    image.setAttribute('src', imageUrl)
+
+    //append image to page
+    imageDiv.appendChild(image)
+    gifs.appendChild(imageDiv)
+
 }
 
 
 //AJAX REQUEST 
-var ajaxUrl = 'http://api.giphy.com/v1//random?api_key=dc6zaTOxFJmzC&tag=american+psycho';
-var request = new XMLHttpRequest();
-request.open('GET', ajaxUrl, true);
+function ajaxCall(){
+  var ajaxUrl = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho';
+  var request = new XMLHttpRequest();
+  request.open('GET', ajaxUrl, true);
 
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    var resp = request.responseText;
-    loadGifs(resp)
-  } else {
-    // We reached our target server, but it returned an error
-    console.log(err)
-  }
-};
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      var resp = JSON.parse(request.responseText);
+      console.log(resp)
+      loadGifs(resp)
+    } else {
+      // We reached our target server, but it returned an error
+      console.log(err)
+    }
+  };
+  request.send();
+}
 
-request.send();
+//load images
+for (var i = 0; i < 5; i++) ajaxCall()
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 var http = require('http');
 var fs = require('fs');
 
-function readFile(res, filename){
-  fs.readFile(filename, function(err, data){
-    res.write(data.toString());
-    res.end();
-  })
-}
-
-
 var server = http.createServer(function (req, res){
+  
+  function readFile(filename){
+    fs.readFile(filename, function(err, data){
+      res.write(data);
+      res.end();
+    })
+  }
+
   var url = req.url;
 
-  if(url === '/') readFile(res, 'index.html');
-  else if (url === '/page-2') readFile(res, 'page-2.html');
-  else if (url === '/page-3') readFile(res, 'page-3.html');
-  else if (url === '/main.css') readFile(res, 'main.css');
-  else if (url ==='/app.js') readFile(res, 'app.js');
+  if(url === '/') readFile('index.html');
+  else if (url === '/random') readFile('random.html');
+  else if (url === '/search') readFile('search.html');
+  else if (url === '/main.css') readFile('main.css');
+  else if (url ==='/app.js') readFile('app.js');
   else {
     res.write('<h1>404</h1>')
     res.end();
